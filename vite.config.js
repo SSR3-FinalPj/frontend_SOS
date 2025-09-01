bfd27f1b1493cc9b4196e850dbf901018dd7aafc
WorkSpaceForHanmg <dlzkfmtm11@naver.com>
2025-09-01 17:50:56 +0900

    Feat : env 넣기
    백엔드 URL env로 넣기

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src'
+export default defineConfig(({ mode }) => {
+  // .env 파일 로드
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src'
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // 백엔드 주소
-        changeOrigin: true,              // Origin 헤더 변경
-        secure: false,                   // HTTPS 인증서 무시
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL, // ✅ 여기서 .env 값을 가져옴
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
