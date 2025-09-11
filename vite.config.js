7ebdffd27b2aa6f560b95b71c4f46455568b5b3d
Kanghyeokjie <kanghyeokjie@gmail.com>
2025-09-11 09:47:47 +0900

    fix: 사용하지 않는 svgr 관련 코드 삭제

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
 
 export default defineConfig(({ mode }) => {
   // .env 파일 로드
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [react(), svgr()],
+    plugins: [react()],
     resolve: {
       alias: {
         '@': '/src'
